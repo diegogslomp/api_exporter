aebfd846eaf8262d568cb2a976b46bae9fbb9b1f
_ <diegogslomp@gmail.com>
2022-11-21 21:16:31 -0300

    raise error for api responses, add dummy tests

--- sitrad_exporter.py
+++ sitrad_exporter.py
@@ -1,9 +1,10 @@
 from prometheus_client import start_http_server, Gauge
+from dotenv import load_dotenv
 import requests
 import urllib3
-import os
+import logging
 import time
-from dotenv import load_dotenv
+import os
 
 
 class Sensor:
@@ -12,80 +13,71 @@ class Sensor:
     def __init__(self, sensor: dict):
         self.id = sensor["id"]
         self.name = sensor["name"]
-        self.gauge = Gauge(self.gauge_name(), f"{self.name}")
+        self.gauge = Gauge(self.get_gauge_name(), f"{self.name}")
 
-    def gauge_name(self) -> str:
+    def get_gauge_name(self) -> str:
         sanitized_name = self.name.lstrip().lower().replace(" ", "_")
         return f"{sanitized_name}_temperature_api_requests"
 
 
 class API:
-    """SITRAD API request methods"""
+    """Sitrad API request methods"""
 
     @staticmethod
     def get_response(path) -> object:
         """Return response from API"""
-
         host = os.getenv("API_HOST")
         port = os.getenv("API_PORT")
         user = os.getenv("API_USER")
         password = os.getenv("API_PASSWORD")
         url = f"https://{host}:{port}/api/v1/{path}"
-
-        return requests.get(
+        
+        response = requests.get(
             url,
             auth=(user, password),
             verify=False,
         )
+        response.raise_for_status()
+        return response
 
     @staticmethod
     def get_sensors() -> list[dict]:
         """Query API and filter sensors"""
         response = API.get_response(f"instruments")
-
-        if response.status_code == 200:
-            return [
-                {"id": result["id"], "name": result["name"]}
-                for result in response.json()["results"]
-            ]
+        return [
+            {"id": result["id"], "name": result["name"]}
+            for result in response.json()["results"]
+        ]
 
     @staticmethod
     def get_temperature(sensor_id: int) -> float:
         """Query API and filter sensor temperature value"""
-
         response = API.get_response(f"instruments/{sensor_id}/values")
-
-        if response.status_code == 200:
-            for result in response.json()["results"]:
-                if result["code"] == "Temperature":
-                    return result["values"][0]["value"]
+        for result in response.json()["results"]:
+            if result["code"] == "Temperature":
+                return result["values"][0]["value"]
 
 
 def process_request() -> None:
     """Set Prometheus values based on API response every 60 seconds"""
-
     sensors = []
-
     for sensor in API.get_sensors():
         sensors.append(Sensor(sensor))
-
     while True:
         try:
             for sensor in sensors:
                 sensor.gauge.set(API.get_temperature(sensor.id))
         except Exception as ex:
-            print(ex)
+            logging.error(ex)
         finally:
             time.sleep(60)
 
 
 if __name__ == "__main__":
-
     # Disable cert warnings
     urllib3.disable_warnings(urllib3.exceptions.InsecureRequestWarning)
     # Load enviroment variables from .env file
     load_dotenv()
-
     # Start up the server to expose the metrics.
     start_http_server(8083)
     process_request()

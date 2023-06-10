7416c225678b8347f598034829253a63278f7df2
Diego Gobbi Slomp <diegogslomp@gmail.com>
2023-06-10 10:58:55 -0300

    Change wrong sensors to offline, rename tests, add assert to offline sensors

--- exporter/test.py
+++ exporter/test.py
@@ -3,13 +3,15 @@ from sensor import generate_sensor_list, Sensor
 
 
 class TestGauges(unittest.TestCase):
-    def test_generate_sensor_list_and_get_values(self):
+    def test_get_api_sensor_list_and_set_gauge_values(self):
         sensors = generate_sensor_list()
         [sensor.set_gauge_value() for sensor in sensors]
 
-    def test_wrong_sensor(self):
-        wrong_sensor = Sensor(id=140, name="Wrong Sensor")
-        wrong_sensor.set_gauge_value()
+    def test_offline_sensor(self):
+        offline_sensor = Sensor(id=999, name="Offline Sensor")
+        offline_sensor.set_gauge_value()
+        msg = "Offline sensors should be zero"
+        self.assertEqual(offline_sensor.gauge._value.get(), 0.0, msg)
 
 
 if __name__ == "__main__":

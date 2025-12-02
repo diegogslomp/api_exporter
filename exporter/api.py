7bbf1c924999729801c4d9cc2eed71ee8bd8ed91
Diego Gobbi Slomp <diegogslomp@gmail.com>
2025-12-02 18:44:58 -0300

    Add timeout

--- exporter/api.py
+++ exporter/api.py
@@ -9,15 +9,14 @@ dotenv.load_dotenv()
 def get_api_results(url_suffix: str) -> list[dict]:
     # Disable certifcate warnings
     urllib3.disable_warnings(urllib3.exceptions.InsecureRequestWarning)
-    host = os.environ["API_HOST"]
-    port = os.environ["API_PORT"]
-    user = os.environ["API_USER"]
-    password = os.environ["API_PASSWORD"]
-    url = f"https://{host}:{port}/api/v1/{url_suffix}"
+    url = (
+        f"https://{os.environ["API_HOST"]}:{os.environ["API_PORT"]}/api/v1/{url_suffix}"
+    )
     response = requests.get(
         url=url,
-        auth=(user, password),
+        auth=(os.environ["API_USER"], os.environ["API_PASSWORD"]),
         verify=False,
+        timeout=float(os.getenv("TIMEOUT", 5)),
     )
     response.raise_for_status()
     return response.json()["results"]
